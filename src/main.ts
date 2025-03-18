ee416987451a069032e47de3a18f49f4c8fed81b
Michallesss <mwiemszra@gmail.com>
2025-03-18 19:39:19 +0100

    ➕ Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // const corsOptions = {
+  //   origin: process.env.FRONTEND_URL,
+  //   credentials: true,
+  // };
+  // app.enableCors(corsOptions);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const swagger = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swagger());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
