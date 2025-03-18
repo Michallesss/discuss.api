766df5685033420fa4f160a4fb7935163df29ea5
Michallesss <mwiemszra@gmail.com>
2025-03-18 19:30:47 +0100

    ➕ Add prisma library

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService/*, PrismaService*/], // Prisma service must be initialized with db at first
 })
 export class AppModule {}
