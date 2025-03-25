f57e98fffdd907e7cb0e074091d9bb26ac125c73
Michallesss <mwiemszra@gmail.com>
2025-03-25 22:05:58 +0100

    🥚 Add app information route

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getInfo(): any {
+    return {
+      name: 'Discuss.it',
+      version: '0.1.0',
+      description: null,
+    };
   }
 }
