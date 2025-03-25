3eae9b95d6509efbdfd45d638edf62c2c2416e68
Michallesss <mwiemszra@gmail.com>
2025-03-25 22:26:22 +0100

    🛂 login and register (beta)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, UsePipes } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ZodValidationPipe } from 'src/zod.pipe';
+import { loginSchema, loginType, registerSchema, registerType } from './schemas/sign.schemas';
+
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  // Public decorator
+  @Post('login')
+  @UsePipes(new ZodValidationPipe(loginSchema))
+  signIn(@Body() body: loginType) {
+    return this.authService.signIn(body.email, body.password);
+  }
+
+  @HttpCode(HttpStatus.CREATED)
+  // Public decorator
+  @Post('register')
+  @UsePipes(new ZodValidationPipe(registerSchema))
+  signUp(@Body() body: registerType) {
+    return this.authService.signUp(body.email, body.password);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
 
-  async signIn(email: string, password: string) {
-    // https://docs.nestjs.com/recipes/prisma#use-prisma-client-in-your-nestjs-services
-    // https://github.com/Michallesss/PrintAPI/blob/main/src/auth/auth.service.ts
+  async signIn(email: string, pass: string) {
+    const user = await this.usersService.findOne({ email: email });
+
+    if (!user) throw new NotFoundException('Email or password is incorrect');
+    if (user.password !== pass) throw new NotFoundException('Email or password is incorrect');
+
+    const { password, ...result } = user;
+    // TODO: Generate a JWT and return it here
+    // instead of the user object
+    return result;
   }
 
-  // async signUp(email: string, password: string) {}
+  async signUp(email: string, password: string) {
+    const user = await this.usersService.findOne({ email: email });
+
+    if (user) throw new ConflictException('User already exists');
+
+    // TODO: Generate a JWT and return it here
+    // instead of the user object
+    return this.usersService.create({ email, password });
+  }
 }
